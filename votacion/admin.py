ef4668c1704d01249997371af556e9fcee0ea0a7
Victor Maya <vimahy@yahoo.com.mx>
2018-06-07 17:01:23 -0500

    Los dos modelos en el sistema de administracion

--- votacion/admin.py
+++ votacion/admin.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Usuario, Informe
+
+
+
+admin.site.register(Usuario)
+admin.site.register(Informe)
+
